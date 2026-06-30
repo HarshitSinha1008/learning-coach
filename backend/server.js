964503233890a112faa36981bec5a3e554f76035
HarshitSinha1008 <harshitsinha55@gmail.com>
2026-07-01 02:53:58 +0530

    Connect Express backend with FastAPI memory service

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios');
 
 const app = express();
 
@@ -12,11 +13,31 @@ app.get('/hello', (req, res) => {
     res.send("Hello Harshit");
 });
 
+app.get("/test-python", async(req, res) => {
+    try {
+        const response = await axios.get("http://127.0.0.1:8000/");
+        res.json(response.data);
+    }catch (error) {
+        console.error(error);
+        res.status(500).json({
+            message: "Could not connect to Python Server"
+        });
+    }
+});
+
 app.post('/profile', (req, res) => {
-    console.log(req.body);
+
+    const goal = req.body.goal;
+    const learningStyle = req.body.learningStyle;
+
+    console.log(goal);
+    console.log(learningStyle);
+    
     res.json({
         message: "Profile recived successfully"});
 });
+
+
 
 app.listen(3000, () => {
     console.log("Learning Coach Backend is Running");
