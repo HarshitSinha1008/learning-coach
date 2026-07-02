903b9b55ae53c405c988f942866b32f1f20924af
HarshitSinha1008 <harshitsinha55@gmail.com>
2026-07-03 01:43:25 +0530

    made fast api server

--- memory-service/main.py
+++ memory-service/main.py
@@ -7,3 +7,19 @@ def home():
     return {
         "message": "Memory Service is running"
     }
+
+@app.post("/remember-profile")
+def remember_profile(profile: dict):
+
+    goal = profile["goal"]
+    learning_style = profile["learningStyle"]
+    preferred_explanation = profile["preferredExplanation"]
+
+    print("Goal:", goal)
+    print("Learning Style:", learning_style)
+    print("Preferred Explanation:", preferred_explanation)
+
+    return {
+        "status": "success",
+        "message": "Profile stored successfully"
+    }
